0f593f10c663a68e8a802cc3baa62fe54fb23e69
James Gibbel <jamesgibbel@gmail.com>
2019-08-25 23:50:45 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -214,4 +214,84 @@ def big_shoe_rebounds()
   end
   bigbounds
 end
+def most_points_scored()
+  big_hash = game_hash()
+  player = 0 
+  points = 0 
+  big_hash.each do |loc, data|
+    data.each do |k1, v1|
+      if k1 == :players
+        v1.each do |k2, v2|
+          if k2[:points] > points
+            player = k2[:player_name]
+            points = k2[:points]
+          end
+        end
+      end
+    end
+  end
+  player
+end
 
+def winning_team
+    hash = game_hash
+    home_points = 0
+    away_points = 0
+    home_team = hash[:home][:team_name]
+    away_team = hash[:away][:team_name]
+    hash.each do |loc, info|
+        info.each do |key, value|            
+            if key == :players
+                value.each do |k, v|
+                    if loc == :home
+                        home_points += k[:points]
+                    else 
+                        away_points += k[:points]
+                    end
+                end
+            end
+        end
+    end
+    home_points > away_points ? home_team : away_team
+end
+  
+def player_with_longest_name()
+  big_hash = game_hash()
+  player = 0 
+  length = 0 
+  big_hash.each do |loc, data|
+    data.each do |k1, v1|
+      if k1 == :players
+        v1.each do |k2, v2|
+          name = k2[:player_name]
+          if name.size > length
+            player = k2[:player_name]
+            length = name.size
+          end
+        end
+      end
+    end
+  end
+  player
+end
+
+def long_name_steals_a_ton?()
+  big_hash = game_hash()
+  player = 0 
+  steal = 0
+  player_long = player_with_longest_name()
+  big_hash.each do |loc, data|
+    data.each do |k1, v1|
+      if k1 == :players
+        v1.each do |k2, v2|
+          name = k2[:player_name]
+          if k2[:steals] > steal
+            player = k2[:player_name]
+            steal = k2[:steals]
+          end
+        end
+      end
+    end
+  end
+  player_long == player ? true : false
+end
